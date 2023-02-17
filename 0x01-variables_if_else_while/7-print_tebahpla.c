edadb9dbf9cf6a3ff8784786f6c12ad0a2e234c1
Allan Thuranira <allanthura1@gmail.com>
2023-02-17 14:29:28 -0800

    print tebah

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char s;
+
+	for (s = 'z'; s >= 'a'; s--)
+		putchar(s);
+
+	putchar('\n');
+
+	return (0);
+}
+
