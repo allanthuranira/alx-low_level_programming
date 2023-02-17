095eea20152fdadfeb2ff8bd3dae806cf1613828
Allan Thuranira <allanthura1@gmail.com>
2023-02-17 14:31:47 -0800

    print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int s;
+	int u;
+
+	for (s = 48; s <= 57; s++)
+	{
+		putchar(s);
+	}
+	for (s = 65; s <= 70; s++)
+	{
+		u = tolower(s);
+		putchar(u);
+	}
+	putchar('\n');
+
+	return (0);
+}
