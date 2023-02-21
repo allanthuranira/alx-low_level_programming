b66d16269a561ed51bd8e5257792920a512572bc
allanthuranira <allanthura1@gmail.com>
2023-02-21 16:01:45 +0000

    put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include <main.h>
 
 int main () {
 
 	        char putchar[7] = "putchar";
 
-		        putchar(putchar);
+		        putchar(putchar "%s\n");
 
 			        return(0);
 
