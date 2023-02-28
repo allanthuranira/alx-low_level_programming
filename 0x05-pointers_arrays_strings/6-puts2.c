123a38289d457af9c54efdd18792b1aca368e1b6
allanthuranira <allanthura1@gmail.com>
2023-02-28 09:54:01 +0000

    print every character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* puts2 - function prints every character of a string
+* @str: input string
+* Return: nothing
+*/
+
+void puts2(char *str)
+{
+	int n = 0, m;
+
+	while (str[n] != '\0')
+		n++;
+
+		n -= 1;
+
+	for (m = 0; m <= n; m += 2)
+	_putchar(str[m]);
+
+	_putchar('\n');
+}
