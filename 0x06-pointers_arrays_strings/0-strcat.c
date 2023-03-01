73229156dd8e0368e3a4eccee8ae020ca2ff94cf
allanthuranira <allanthura1@gmail.com>
2023-03-01 13:22:31 +0000

    concatenate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/** for concatenating two strings
+ * string char
+ * string dest
+ */
+int main (void)
+{
+	char src[7] = "Hello ";
+	char dest[7] = "World!\n";
+	char *ptr;
+	_putchar ("%s\n", strtcat(src*, dest* ));
+	return 0;
