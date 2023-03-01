6da05b4e06c1a4627acdf4a4505512b83af384ff
allanthuranira <allanthura1@gmail.com>
2023-03-01 17:23:30 +0000

    concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,6 +7,8 @@ int main (void)
 {
 	char src[7] = "Hello ";
 	char dest[7] = "World!\n";
-	char *ptr;
-	_putchar ("%s\n", strtcat(src*, dest* ));
+	char *ptr = src;
+	_strcat(src,dest);
+	_putchar (*ptr);
+	_putchar ("%s\n", src);
 	return 0;
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 #include <stdio.h>
-char *_strcat(char *dest, char *src);
-int putchar (int character);
+char *_strcat(char dest, char src);
+char _putchar (char character);
+char *_putchar (char character);
