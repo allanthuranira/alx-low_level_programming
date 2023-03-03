a334003488e66a4178e3572090ee36475894549b
allanthuranira <allanthura1@gmail.com>
2023-03-03 07:52:32 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,8 +10,9 @@
 char *_strcat(char *dest, char *src)
 {
 
-	char dest [7] = {'h','e','l','l','o', ' '};
-	char src [7] = {'w','o','r','l','d','!','\n'};
+	char dest [7] = "hello\0";
+	char src [7] = "world!\n";
+	char *dest = strcat(char dest, char src);
 	char *ptr = *dest;
 	_putchar(*dest);
 	_putchar(*ptr);
